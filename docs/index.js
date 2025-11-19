d89e0cbd52e5ec3ae24d94cf740c1d71faaf0325
Cyra Uthoff <196216906+uthoffcyra@users.noreply.github.com>
2025-11-19 16:05:50 -0500

    Files & Styles

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,13 @@
 // Fugue Web Environment
 var computer;
 
+let default_settings = `{
+    [ "list.show_hidden" ] = false,
+    [ "motd.path" ] = ".fugue.motd",
+}`;
+
+let fugue_motd = `Welcome to Fugue Lang!`;
+
 // Tell requirejs to load copycat/* from the website.
 require.config({ paths: { copycat: "https://copy-cat.squiddev.cc/" } });
 // Find our #embed-computer element and inject a computer terminal into it.
@@ -8,6 +15,10 @@ require(["copycat/embed"], setup => {
     setup(document.getElementById("embed-computer"), {
         width: 80,
         height: 30,
+        files: {
+            ".settings": default_settings,
+            ".fugue.motd": fugue_motd
+        }
     }).then((res) => {
         computer = res;
     });
