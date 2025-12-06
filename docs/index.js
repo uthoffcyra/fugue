48d4177acac489fa67e2dd17d12f2e95dc83d8f9
Cyra Uthoff <196216906+uthoffcyra@users.noreply.github.com>
2025-12-06 12:12:46 -0500

    Update Site

--- docs/index.js
+++ docs/index.js
@@ -1,35 +1,35 @@
 // Fugue Web Environment
 var computer;
+var files = {};
 
-var files = {
-    '.settings': `{
-        [ "list.show_hidden" ] = false,
-        [ "motd.path" ] = ".fugue.motd",
-    }`,
-    '.fugue.motd': 'Welcome to Fugue Lang!',
-    'startup.lua': `shell.setPath( shell.path()..":/fugue" )`,
-    'examples/test.fe': `~ web test! ~
-    let x = 10;
-    `
-}
-
-var load_these_files = ['fugue_fe.lua','fugue_interp.lua','fugue_lexer.lua',
-    'fugue_lib.lua','fugue_state.lua'];
-let loadreq = load_these_files.length;
-let loaded = 0;
+var load_these_files = [
+    {source_folder:'src', target_folder: 'fugue/',
+    files:['fugue_fe.lua','fugue_interp.lua','fugue_lexer.lua',
+        'fugue_lib.lua','fugue_state.lua','fugue_walk.lua',
+        'fugue_builtin.lua', 'fugue_symtab.lua']},
+    {source_folder:'docs/site-res', target_folder: '',
+    files:['.fugue.motd','.settings','startup.lua']}
+];
 
 function load_interpreter() {
-    load_these_files.forEach((fname) => {
-        fetch('https://raw.githubusercontent.com/uthoffcyra/fugue/refs/heads/main/src/'+fname)
-        .then((response) => response.text().then((body)=>{
-            file_callback(fname, body)
-        }));
+    load_these_files.forEach((folder_object) => {
+
+        folder_object.files.forEach((file_name) => {
+
+            let base = 'https://raw.githubusercontent.com/uthoffcyra/fugue/';
+            let source_folder = folder_object.source_folder;
+            let target_folder = folder_object.target_folder;
+
+            fetch(base+ 'refs/heads/main/'+source_folder+'/'+file_name)
+            .then((response) => response.text().then((body)=>{
+                files[target_folder+file_name] = body;
+            }));
+
+        });
+
     });
-}
-function file_callback(fname,body) {
-    files['fugue/'+fname] = body;
-    loaded+=1;
-    if (loaded == loadreq) load_emulator();
+
+    load_emulator();
 }
 
 function load_emulator() {
@@ -51,7 +51,7 @@ function load_emulator() {
 }
 
 // another option is to make a custom terminal that takes events
-function execute_code() {
+function run_file() {
     computer.reboot()
 
     // set startup file to 1) open desired file... 2) perform desired command
--- docs/site-res/startup.lua
+++ docs/site-res/startup.lua
@@ -5,3 +5,5 @@ local completion = require('cc.shell.completion')
 shell.setCompletionFunction('fugue/fugue_interp.lua',
     completion.build( completion.file ))
 shell.setAlias('fe', 'fugue/fugue_interp.lua')
+
+-- line #9 for loading files with js
